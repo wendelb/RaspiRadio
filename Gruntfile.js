d98f572c1178d05040c453bec5bfb79203fa6d57
Bernhard Wendel <BWendel@gmx.de>
2015-11-03 22:40:17 +0100

    Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+module.exports = function (grunt) {
+
+    // load the plugins.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks("grunt-jscs");
+
+    // project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // JS-Development
+        jshint: {
+            allFiles: ['*.js', 'routes/*.js', 'public/js/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        jscs: {
+            src: ['*.js', 'routes/*.js', 'public/js/*.js'],
+            options: {
+                config: '.jscsrc'
+            }
+        }
+    });
+
+    // configure tasks.
+    // Development-Tasks
+    grunt.registerTask('default', ['hint', 'cs']);
+    grunt.registerTask('hint', ['jshint']);
+    grunt.registerTask('cs', ['jscs']);
+};
