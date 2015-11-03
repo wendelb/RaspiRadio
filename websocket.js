0192e2c8a5e2b72e3ee0179d9a2db994c5ecc582
Bernhard Wendel <BWendel@gmx.de>
2015-11-03 23:15:52 +0100

    Socket.IO-Server

---
+++ websocket.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+var socket = require('socket.io'),
+    debug = require('debug')('mpc:websocket'),
+    MPDAdapter = require('mpdadapter');
+
+function SocketIOServer(http) {
+    var io = socket(http),
+        client = new MPDAdapter('192.168.2.100');
+
+    io.on('connection', function (socket) {
+        debug('A client connected');
+
+        // Init für diesen Client durchführen
+        socket.emit('playerstatus', client.status.state);
+        socket.emit('newSong', client.currentsong);
+    });
+
+    client.on('ready', function () {
+        debug('MPD initiated: %s', client.currentsong.Title);
+    });
+
+    client.on('songinfo', function (newSong) {
+        debug('MPD new song: %s', newSong.Title);
+        io.emit('newSong', newSong);
+    });
+}
+
+module.exports = exports = SocketIOServer;
