4dd1825825e6ac1cd668ef4ed8c803d0a62b67dd
Bernhard Wendel <BWendel@gmx.de>
2015-11-04 22:59:58 +0100

    first version of the web frontend

---
+++ public/javascripts/script.js
@@ -0,0 +1,42 @@
+﻿"use strict";
+
+$(function () {
+    // Onload
+
+    var socket = io.connect();
+    socket.on('playerstatus', function (data) {
+        if (data === "play") {
+            $(".radio-on").show();
+            $(".radio-off").hide();
+
+            $(".toggle-radio").parent().addClass("active");
+        }
+        else {
+            $(".radio-on").hide();
+            $(".radio-off").show();
+
+            $(".toggle-radio").parent().removeClass("active");
+        }
+    });
+    socket.on('newSong', function (data) {
+        $(".item-stream").text(data.Name);
+
+        // Sofern sich der Titel geändern hat: updaten
+        if ($(".item-titel").text() !== data.Title) {
+            $(".item-titel").text(data.Title);
+
+            // Marquee starten/aktualisieren
+            $('.marquee').marquee({
+                duplicated: true,
+                duration: 5000
+            });
+        }
+        console.log(data.Title);
+    });
+
+
+    $(".toggle-radio").add(".radio-icon img").click(function () {
+        // Toggle radio
+        socket.emit("toggleRadio");
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'RasPi Radio', script: '<script src="/javascripts/script.js"></script>' });
 });
 
 module.exports = router;
